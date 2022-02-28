70e603b1937aa6d25167acad12bd64c4ea02c253
DDD91 <ghecci1614@gmail.com>
2022-02-28 10:01:24 -0500

    Added link to Github, removed logo/added react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    <h1> Weather App </h1>
+    <footer> 
+      This project was coded by Devanie Dawson and is {" "} 
+    <a 
+    href= "https://github.com/DDD91/my-weather-react" 
+    target="_blank"
+     >
+      open-sourced on Github
+    </a>
+     </footer>
+    </div>
+  );
 
 }
 
