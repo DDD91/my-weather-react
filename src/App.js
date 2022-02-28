6fc2836556aece9bc9ca84c3a28f83dee3a7dcf2
DDD91 <ghecci1614@gmail.com>
2022-02-28 15:28:39 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 export default function App() {
   return (
   <div className="App">
-    <h1> Weather App </h1>
+    <div className="container">
+    <h1>Weather App</h1>
     <footer> 
       This project was coded by Devanie Dawson and is {" "} 
     <a 
@@ -15,6 +16,7 @@ export default function App() {
       open-sourced on Github
     </a>
      </footer>
+     </div>
     </div>
   );
 
