6dcf00e3b96fc2ce1be35c315f9db3867141748e
DDD91 <ghecci1614@gmail.com>
2022-03-07 11:40:58 -0500

    Created new component & tracked to give correct temperature output

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,17 +1,17 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return  ( 
-        <div className="WeatherInfo">
-    <h1>{props.data.city}</h1>
-    <ul>
-        <li>
+    <div className="WeatherInfo">
+         <h1>{props.data.city}</h1>
+         <ul>
+          <li>
             <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}
-        </li>
+        <li className="text-capitalize">{props.data.description}</li>
     </ul>
     <div className="row mt-3">
         <div className="col-6">
@@ -19,15 +19,14 @@ export default function WeatherInfo(props) {
                 <div className="float-left">
                  <WeatherIcon code={props.data.icon} />
             </div>
+    
+        
            
             <div className="float-left">
-                <div className="WeatherTemperature">
-            <span className="temperature">{Math.round(props.data.temperature)}</span>
-            <span className="unit">°C</span>
-             </div>
-            </div>
+                <WeatherTemperature celsius={props.data.temperature} 
+                />
+           </div>
           </div>
-        </div>
             <div className="col-6">
                 <ul>
                      
@@ -39,7 +38,8 @@ export default function WeatherInfo(props) {
                     </li>
                 </ul>
             </div>
-    </div>
-    </div>
+        </div>
+        </div>
+        </div>
     );
 }
