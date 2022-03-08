3f93710a7e0144fe8eb7e3610dfd39f2ba187886
DDD91 <ghecci1614@gmail.com>
2022-03-07 19:47:07 -0500

    Made necessary edits (& also testing edits)

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -28,7 +28,7 @@ export default function WeatherIcon(props) {
 
      return (
      <ReactAnimatedWeather
-     icon={codeMapping(props.code)}
+     icon={codeMapping[props.code]}
      color="black"
      size={52}
      animate={true}
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -18,11 +18,11 @@ export default function WeatherTemperature(props) {
 
    if (unit === "celsius") {
     return (
-        <div className="WeatherTemperature">
-            <span className="temperature">{Math.round(props.celsius)}
-                </span>
+        <div className="weatherTemperature">
+            <span className="temperature">{Math.round(props.celsius)}</
+            span>
                 <span className="unit">
-                    °C |{""}
+                    °C |{" "}
                     <a href="/" onClick={showFahrenheit}>
                         °F
                         </a>
@@ -32,11 +32,12 @@ export default function WeatherTemperature(props) {
            } else {
              return (
                 <div className="WeatherTemperature">
-                <span className="temperature">{Math.round(fahrenheit())}</span>
+                <span className="temperature">{Math.round(fahrenheit())}</
+                span>
                     <span className="unit">
                        <a href="/" onClick={showCelsius}>
                            °C
-                           </a>{""} 
+                           </a> {" "} 
                            | °F
                            </span>
                     </div>
